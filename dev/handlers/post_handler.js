1b42f1c72fc6a3b2e05abc26ae6083cfe15411f5
honehone <honehone.41games@gmail.com>
2025-01-15 17:05:01 +0900

    added post handler

--- dev/handlers/post_handler.js
+++ dev/handlers/post_handler.js
@@ -1,3 +1,21 @@
 // @ts-check
 'use strict'
 
+import { OK } from "../../index.js";
+
+/**
+ * @param {{isBase64Encoded: boolean, body: string}} event 
+ * @param {any} context,
+ * @returns {Promise<{
+ *  statusCode: number, 
+ *  headers: any, 
+ *  body: string
+ * }>} 
+ */
+export async function handler(event, context) {
+    return {
+        statusCode: OK,
+        headers: {},
+        body: "ok"
+    };
+}
--- dev/main.js
+++ dev/main.js
@@ -3,12 +3,15 @@
 
 import {mazu, fastify, log} from '../index.js';
 import {handler as getHandler} from './handlers/get_handler.js';
-
-mazu.init();
-mazu.get('/', getHandler);
+import { handler as postHandler } from './handlers/post_handler.js';
 
 try {
-    fastify.listen({port: 3000});
+    mazu.init();
+    mazu.get('/', getHandler);
+    mazu.post('/', postHandler);
+
+    const port = 3000;
+    await fastify.listen({port});
 } catch (error) {
     log.error(error);
     process.exit(1);
