82f84a54306c53b0f643c2d75745d644a27828d0
honehone <honehone.41games@gmail.com>
2025-01-16 15:20:54 +0900

    added parameter decode for post

--- dev/handlers/post_handler.js
+++ dev/handlers/post_handler.js
@@ -1,7 +1,8 @@
 // @ts-check
 'use strict'
 
-import { log, OK } from "../../index.js";
+import { log, OK, badRequest } from "../../index.js";
+import querystring from "node:querystring";
 
 /**
  * @param {{isBase64Encoded: boolean, body: string}} event 
@@ -9,15 +10,30 @@ import { log, OK } from "../../index.js";
  * @returns {Promise<{
  *  statusCode: number, 
  *  headers: any, 
- *  body: string
+ *  body: any
  * }>} 
  */
 export async function handler(event, context) {
-    if (event.body) {
-        log.info(event.body);
-        log.info(event.isBase64Encoded);
+    if (!event.body) {
+        return badRequest();
     }
 
+    try {
+        log.info('raw: %s', event.body);
+        let body = event.body;
+        if (event.isBase64Encoded) {
+            const b64Dec = Buffer.from(event.body, 'base64')
+                .toString('utf-8');
+            log.info("b64 decoded: %s", b64Dec);
+            body = b64Dec;
+        }
+
+        const query = querystring.parse(body);
+        log.info('query: %o', query);    
+    } catch (error) {
+        return badRequest();
+    }
+    
     return {
         statusCode: OK,
         headers: {
